76c5b10888f0872b0c53076c09ee1ec106e8c02c
Oliver <oliver@MSI>
2022-12-31 08:55:40 -0800

    finished 4/4 problems

--- reduce.js
+++ reduce.js
@@ -1,15 +1,24 @@
 /*
-Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
+Write a function called extractValue which accepts an array of objects and 
+a key and returns a new array with the value of each object at the key.
 
 Examples:
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+    return arr.reduce(function(acc,obj){
+        acc.push(obj[key]);
+        return acc;
+    },[])
+}
 
 /*
-Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
+Write a function called vowelCount which accepts a string and returns an 
+object with the keys as the vowel and the values as the number of times 
+the vowel appears in the string. This function should be case insensitive 
+so a lowercase letter and uppercase letter should count
 
 Examples:
     vowelCount('Elie') // {e:2,i:1};
@@ -19,10 +28,27 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+    const vowels = 'aeiou';
+    const strArr = Array.from(str.toLowerCase());
+    console.log (strArr);
+
+    return strArr.reduce(function(obj,letter){
+        if (vowels.indexOf(letter) !== -1){
+            if (obj[letter]){
+                obj[letter] ++;
+            } else {
+                obj[letter] = 1;
+            }
+        } return obj;
+    },{})
+}
+
 
 /*
-Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
+Write a function called addKeyAndValue which accepts an array of objects 
+and returns the array of objects passed to it with each object now including 
+the key and value passed to the function.
 
 Examples:
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
@@ -36,10 +62,21 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+    return arr.reduce(function(acc,obj,index){   
+        acc[index][key] = value;
+        return acc;
+    },arr)
+}
 
 /*
-Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
+Write a function called partition which accepts an array and a callback 
+and returns an array with two arrays inside of it. The partition function 
+should run the callback function on each value in the array and if 
+the result of the callback function at that specific value is true, the 
+value should be placed in the first subarray. If the result of the callback 
+function at that specific value is false, the value should be placed in the 
+second subarray. 
 
 Examples:
     
@@ -60,4 +97,13 @@ Examples:
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback) {}
+function partition(arr, callback) {
+    return arr.reduce(function(acc,val){
+        if (callback(val)){
+            acc[0].push(val);
+        } else {
+            acc[1].push(val);
+        }
+        return acc;
+    },[[],[]])
+}
